d8428340de4a5aa1a517b380ccade5f5d39d43de
Danuja Silva <danuja.kowaski@gmail.com>
2022-05-09 21:02:07 +0530

    Added basic layout of the backend

---
+++ database/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
+    .then(() => { console.log("DB Connected uccessfully!") })
+    .catch( (error) =>  { console.log(" DB connected unsuccessfully " , error) });
+
+module.exports = mongoose;
